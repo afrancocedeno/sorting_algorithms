9696ad76641d4895e6373f016cbe430837564606
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-02-01 10:50:00 -0500

    00 | file create | first output done

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * bubble_sort - alforithm with bubble sorting method
+ * @array:
+ * @size:
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, cmp = 0, aux = 0;
+
+	/* traverse the array until null */
+	for (i = 0; i < (size - 1); i++)
+	{
+		/* We need to sort the elements from size - 1 */
+		for (cmp = 0; cmp < (size - 1 - i); cmp++)
+		{
+			/* if the next position is not greater than current */
+			if (*(array + cmp) > *(array + cmp + 1))
+			{
+				/* aux = next */
+				aux = *(array + cmp);
+				/* next = current*/
+				/* current = aux */
+				*(array + cmp) = *(array + cmp + 1);
+				*(array + cmp + 1) = aux;
+				print_array(array, size);
+			}
+		}
+	}
+}
